d780031663cf4335daf622a0aa4b040eebc83dec
Martin Edvardsen <medv@itu.dk>
2017-05-09 00:55:05 +0200

    Firebase hooked up to Facebook user login (android)

--- index.android.js
+++ index.android.js
@@ -12,17 +12,37 @@ import {
   View,
   TouchableOpacity
 } from 'react-native';
-import FBSDK, {LoginManager} from 'react-native-fbsdk'
+import FBSDK, {LoginManager, AccessToken} from 'react-native-fbsdk';
+import firebase from 'firebase';
+
+var config = {
+  apiKey: 'AIzaSyC0ebOlU07pnzkS0VhLrOQGgJDgQnj85UE',
+  authDomain: 'earshare-database.firebaseapp.com/',
+  databaseURL: 'https://earshare-database.firebaseio.com/'
+}
+
+const firebaseRef = firebase.initializeApp(config);
 
 export default class earshareapp extends Component {
   _fbAuth() {
-    LoginManager.logInWithReadPermissions(['public_profile']).then(function(result) {
+    LoginManager.logInWithReadPermissions(['public_profile', 'email']).then(function(result) {
       if (result.isCancelled) {
         console.log('Login was cancelled');
       } else {
-        console.log('Login was a success' + result.grantedPermissions.toString());
+        AccessToken.getCurrentAccessToken().then((AccessTokenData) => {
+          const credential = firebase.auth.FacebookAuthProvider.credential(AccessTokenData.AccessToken)
+          firebase.auth().signInWithCredential(credential).then((result) => {
+            //Promise succesful
+          }, (error) => {
+            //Promise rejected
+            console.log(error)
+          })
+        }, (error) => {
+          console.log('Some error occured: ' + error)
+        })
       }
-    }, function(error) {
+    }, 
+    function(error) {
       console.log('An error occured: ' + error);
     })
   }
@@ -31,7 +51,7 @@ export default class earshareapp extends Component {
     return (
       <View style={styles.container}>
         <TouchableOpacity onPress={this._fbAuth()}>
-
+          <text>Login With Facebook</text>
         </TouchableOpacity>
 
       </View>
